173ff36176ac4839029933a938abe2c57ba6a8ea
ruslan <ruskathefaska2@gmail.com>
2024-08-05 22:52:47 +0500

    written serialization/deserialization

--- include/serializer.h
+++ include/serializer.h
@@ -5,13 +5,43 @@
 #include <string>
 #include <vector>
 
-class Serializer {
- public:
-  Serializer() = default;
-  static void serialize(std::fstream& to, const std::vector<std::string>& data);
-  static std::vector<std::string> deserialize(std::fstream& from);
+namespace  serializer {
+inline void serialize(std::fstream& fs, const std::vector<std::string>& data) {
+  if (!fs.is_open()) {
+    throw std::runtime_error("file for serialize doesn't opened");
+  }
 
- private:
-};
+  const auto obj_len = data.size();
+  fs.write(reinterpret_cast<const char*>(&obj_len), sizeof(obj_len));
+
+  for (const auto& str : data) {
+    const auto len = str.length();
+    fs.write(reinterpret_cast<const char*>(&len), sizeof(len));
+    fs.write(str.data(), len);
+  }
+}
+
+inline std::vector<std::string> deserialize(std::fstream& fs) {
+  if (!fs.is_open()) {
+    throw std::runtime_error("file for serialize doesn't opened");
+  }
+
+  std::vector<std::string> data;
+  size_t obj_len = 0;
+  fs.read(reinterpret_cast<char*>(&obj_len), sizeof(obj_len));
+
+  for (int i = 0; i < obj_len; ++i) {
+    size_t len = 0;
+    fs.read(reinterpret_cast<char*>(&len), sizeof(len));
+
+    std::string t(len, '\0');
+    fs.read(t.data(), len);
+    data.push_back(std::move(t));
+  }
+
+  return data;
+}
+
+}; //  namespace Serializer
 
 #endif  // SERIALIZER_H
--- src/main.cc
+++ src/main.cc
@@ -2,15 +2,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstdint>
 
 #include "../include/serializer.h"
 
 int main(int argc, char** argv) {
-  std::vector<std::string> data = {"ruslan", "qwerty123"};
   std::fstream file;
-  file.open("../tests/testfile.bin", std::ios::binary | std::ios::out);
-  Serializer::serialize(file, data);
+  file.open("../tests/testfile.bin", std::ios::in | std::ios::binary);
+  std::vector<std::string> des_data = serializer::deserialize(file);
 
-  file.close();
+  for (const auto& str: des_data) {
+    std::cout << str << std::endl;
+  }
+
   return 0;
 }
